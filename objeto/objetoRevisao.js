83055de123a4f49d12b87f220fe50c5ab6ffce20
brunoleocam <brunoleocam@gmail.com>
2024-01-22 23:28:40 -0300

    Dia 15

--- objeto/objetoRevisao.js
+++ objeto/objetoRevisao.js
@@ -27,5 +27,19 @@ const carro = {
     }, {
         nome: 'Ana',
         idade: 42
-    }]
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
 }
+
+carro.proprietario.endereco.numero = 1000;
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante';
+console.log(carro);
+
+delete carro.condutores;
+delete carro.proprietario.endereco;
+delete carro.calcularValorSeguro;
+console.log(carro);
+console.log(carro.condutores); // undefined
+console.log(carro.condutores.length); // Erro!
